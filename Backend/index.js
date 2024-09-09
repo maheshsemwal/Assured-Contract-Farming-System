6a25f58076fe13a464ce1efee8c5900ca7c98d9b
Mahesh Semwal <maheshsemwal04@gmail.com>
2024-09-09 18:13:36 +0530

    delete some dependencies

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const app = express();
 
 
 dotenv.config();
+connectDB();
+
 app.use(express.json());
 
 
@@ -12,6 +15,8 @@ app.get("/", (req, res)=> {
         msg : "welcome to our server"
     })
 })
+
+
 
 
 const PORT = process.env.PORT || 3000;
